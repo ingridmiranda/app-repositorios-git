9aa36d2d201d2f5f4f8e0d76dc85ca7fa35f86fe
ingridmiranda <ingridmsousa@hotmail.com>
2021-01-08 14:48:11 -0300

    Add animation at listview widget - #7

--- repositoriosGit/lib/app/modules/home/home_page.dart
+++ repositoriosGit/lib/app/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:auto_animated/auto_animated.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -61,15 +62,38 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
       );
   }
 
-  Widget _showInfo() {
-    return Expanded(
-          child: Observer(builder: (_) {
-        return ListView.separated(
-          separatorBuilder:  (BuildContext context, int index) => Divider(),
-          padding: EdgeInsets.only(left:30, right: 30),
-          itemCount: controller.repoInfoList.length,
-          itemBuilder: (context, index) {
-            return Container(
+final options = LiveOptions(
+  delay: Duration(seconds: 0),
+
+  showItemInterval: Duration(milliseconds: 150),
+
+  showItemDuration: Duration(milliseconds: 150),
+
+  // Animations starts at 0.05 visible
+  // item fraction in sight (default 0.025)
+  visibleFraction: 0.05,
+  reAnimateOnVisibility: false,
+);
+
+Widget buildAnimatedItem(
+  BuildContext context,
+  int index,
+  Animation<double> animation,
+) =>
+  // For example wrap with fade transition
+  FadeTransition(
+    opacity: Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(animation),
+    // And slide transition
+    child: SlideTransition(
+      position: Tween<Offset>(
+        begin: Offset(0, -0.1),
+        end: Offset.zero,
+      ).animate(animation),
+      // Paste you Widget
+      child: Container(
               decoration: BoxDecoration(
                 color: Color(0xFFD2E4EE), //Color(0xFFbee3db),//Colors.lightBlue[100], //Colors.amber[200],
                 borderRadius: BorderRadius.circular(10)
@@ -84,8 +108,18 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
                   backgroundImage: NetworkImage(controller.repoInfoList[index].owner.avatar_url,
                 ),
               )),
-            );
-          });
+            ),
+    ),
+  );
+  Widget _showInfo() {
+    return Expanded(
+          child: Observer(builder: (_) {
+        return LiveList.options(
+            options: options,
+            separatorBuilder:  (BuildContext context, int index) => Divider(),
+            padding: EdgeInsets.only(left:30, right: 30),
+            itemCount: controller.repoInfoList.length,
+            itemBuilder: buildAnimatedItem);
       }),
     );
   }
