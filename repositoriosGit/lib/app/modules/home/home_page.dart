e70dd30686b766099485dff0535a7e9b797697a7
ingridmiranda <ingridmsousa@hotmail.com>
2021-01-08 14:04:35 -0300

    Begin page's design - #7

--- repositoriosGit/lib/app/modules/home/home_page.dart
+++ repositoriosGit/lib/app/modules/home/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -24,31 +27,74 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      backgroundColor: Color(0xFF3D5A80), //Color(0xFF89b0ae) , //Colors.lightBlue[900],//Colors.amber[800],
       body: Column(
-        children: <Widget>[_showInfo()],
+        children: <Widget>[
+          _titleText(),
+          _showInfo(),
+          _bottomPage()
+          ],
       ),
     );
+  }
+
+  Widget _titleText(){
+    return Container(
+      padding: EdgeInsets.only(top: 70, left:30, right: 30, bottom: 30),
+      child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Flexible(
+              fit: FlexFit.loose,
+              child: AutoSizeText("Top 100: Primeiros repositórios criados no GitHub", 
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                  ),),
+            ),
+            Image.asset('assets/images/GitHub-Mark-Light-64px.png', scale: 1.5,)
+          ],
+        ),
+      );
   }
 
   Widget _showInfo() {
     return Expanded(
           child: Observer(builder: (_) {
-        return ListView.builder(
+        return ListView.separated(
+          separatorBuilder:  (BuildContext context, int index) => Divider(),
+          padding: EdgeInsets.only(left:30, right: 30),
           itemCount: controller.repoInfoList.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              leading: Image.network(controller.repoInfoList[index].owner.avatar_url),
-              title: Text(controller.repoInfoList[index].name),
-              subtitle: Text(controller.repoInfoList[index].owner.login),
-              onTap: () => _launchURL(controller.repoInfoList[index].html_url),
+            return Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFD2E4EE), //Color(0xFFbee3db),//Colors.lightBlue[100], //Colors.amber[200],
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: ListTile(
+                title: Text(controller.repoInfoList[index].name,  style: TextStyle(color: Color(0xFF293241), fontWeight: FontWeight.w500)),
+                subtitle: Text(controller.repoInfoList[index].owner.login, style: TextStyle(color: Color(0xFF293241))),
+                trailing: Text("#" + (index+1).toString(), style: TextStyle(fontWeight: FontWeight.w700, color: Color(0xFFEE6C4D)/* color: Color(0xFFfaf9f9) */),),
+                onTap: () => _launchURL(controller.repoInfoList[index].html_url),
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(controller.repoInfoList[index].owner.avatar_url,
+                ),
+              )),
             );
           });
       }),
     );
   }
+
+  Widget _bottomPage(){
+      return Padding(
+        padding: EdgeInsets.only(bottom: 30),
+      );
+    }
 }
 
 void _launchURL(String url) async {
