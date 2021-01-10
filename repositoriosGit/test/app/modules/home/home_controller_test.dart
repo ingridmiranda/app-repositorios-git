2a8453eefe4da85a3b66bbf4a47bbce3d8f0707d
ingridmiranda <ingridmsousa@hotmail.com>
2021-01-10 14:31:11 -0300

    Add test to home controller

--- repositoriosGit/test/app/modules/home/home_controller_test.dart
+++ repositoriosGit/test/app/modules/home/home_controller_test.dart
@@ -6,21 +6,33 @@ import 'package:repositoriosGit/app/modules/home/home_module.dart';
 
 void main() {
   initModule(HomeModule());
-  // HomeController home;
-  //
+   HomeController home;
+  
   setUp(() {
-    //     home = HomeModule.to.get<HomeController>();
+         home = HomeModule.to.get<HomeController>();
   });
 
   group('HomeController Test', () {
-    //   test("First Test", () {
-    //     expect(home, isInstanceOf<HomeController>());
-    //   });
+       test("First Test", () {
+         expect(home, isInstanceOf<HomeController>());
+       });
 
-    //   test("Set Value", () {
-    //     expect(home.value, equals(0));
-    //     home.increment();
-    //     expect(home.value, equals(1));
-    //   });
+       test("Set Value", () {
+         expect(home.value, equals(0));
+         home.increment();
+         expect(home.value, equals(1));
+       });
+
+       test("Set Timeout", () {
+         home.setTimeout(true);
+         expect(home.isTimeout, equals(true));
+         home.setTimeout(false);
+         expect(home.isTimeout, equals(false));
+       });
+
+       test("Get 100 repositories", () async{
+         await home.getRepositorios();
+         expect(home.repoInfoList.length, equals(100));
+       });
   });
 }
